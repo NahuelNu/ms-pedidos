53feb4712a2aa825df799b00e3c69a2d81fd7dff
Nahuel Nuñez <83452993+NahuelNu@users.noreply.github.com>
2024-01-03 09:23:05 -0300

    Agregado docker mongo

--- src/main/java/dan/ms/tp/mspedidos/dao/PedidoRepository.java
+++ src/main/java/dan/ms/tp/mspedidos/dao/PedidoRepository.java
@@ -3,14 +3,13 @@ package dan.ms.tp.mspedidos.dao;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import dan.ms.tp.mspedidos.modelo.Pedido;
-
 import java.time.Instant;
 import java.util.List;
 
 @Repository
 public interface PedidoRepository extends MongoRepository<Pedido,String> {
+    
     List<Pedido> findByClienteRazonSocial(String razonSocial);
 
     @Query("{'cliente.razonSocial': ?0, 'fecha': {$gte: ?1, $lte: ?2}}")
